2e70b0776918612fb04a1965e4d223613b7b99d5
jonathanlkw <imjonlam@gmail.com>
2023-04-27 09:12:45 +0800

    Add preliminary draw card function and fix difficult collisions

---
+++ Assets/DrawCardOnValidate.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class DrawCardOnValidate : MonoBehaviour
+{
+    public GameObject card;
+    public Transform spawnPoint;
+    //public float spawnSpeed = 10;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        XRSimpleInteractable interactable = GetComponent<XRSimpleInteractable>();
+        interactable.activated.AddListener(DrawCard);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void DrawCard(ActivateEventArgs arg)
+    {
+        GameObject spawnedCard = Instantiate(card);
+        spawnedCard.transform.position = spawnPoint.position;
+        //spawnedCard.GetComponent<Rigidbody>().velocity = spawnPoint.up   * spawnSpeed;
+    }
+}
