39c132c55625ef5365868c28532094569c6c9367
jonathanlkw <imjonlam@gmail.com>
2023-05-08 01:25:01 +0800

    Add new bell prefab

--- Assets/Bell/PressBellOnValidate.cs
+++ Assets/Bell/PressBellOnValidate.cs
@@ -5,7 +5,7 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 public class PressBellOnValidate : MonoBehaviour
 {
-    public float pushForce = 20;
+    public float pushForce;
     // Start is called before the first frame update
     void Start()
     {
